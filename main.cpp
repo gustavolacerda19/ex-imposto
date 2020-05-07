efb847c3501d9f3c1ceeef902ac78e21fd191bb0
Gustavo Lacerda <gustavolacerda655@gmail.com>
2020-05-07 11:32:32 -0300

    Consolidado de renda

--- main.cpp
+++ main.cpp
@@ -1,21 +1,52 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    double rsalario, rservico, rcapital, gmedicos, geducacionais;
+    double rsalario, rservicos, rcapital, gmedicos, geducacionais, impsalario, impservicos, impcapital;
+    cout << fixed << setprecision(2);
 
     cout << "Renda anual com salario: ";
     cin >> rsalario;
     cout << "Renda anual com prestacao de servico: ";
-    cin >> rservico;
+    cin >> rservicos;
     cout << "Renda anual com ganho de capital: ";
     cin >> rcapital;
     cout << "Gastos medicos: ";
     cin >> gmedicos;
     cout << "Gastos educacionais: ";
     cin >> geducacionais;
+
+    if (rsalario/12 < 3000.0) {
+        impsalario = 0.0;
+    }
+    else if (rsalario/12 >= 3000.0 && rsalario/12 < 5000.0) {
+        impsalario = rsalario * 0.1;
+    }
+    else {
+        impsalario = rsalario * 0.2;
+    }
+
+    if (rservicos != 0.0) {
+        impservicos = rservicos * 0.15;
+    }
+    else {
+        impservicos = 0.0;
+    }
+
+    if (rcapital != 0.0) {
+        impcapital = rcapital * 0.2;
+    }
+    else {
+        impcapital = 0.0;
+    }
+
+    cout << endl << "RELATORIO DE IMPOSTO DE RENDA" << endl << endl;
+    cout << "CONSOLIDADO DE RENDA:" << endl;
+    cout << "Imposto sobre salario: " << impsalario << endl;
+    cout << "Imposto sobre servicos: " << impservicos << endl;
+    cout << "Imposto sobre ganho de capital: " << impcapital << endl;
 
     return 0;
 }
