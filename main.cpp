15ec490f097ce6489190ad3b9c5ba5a95d4911fb
Gustavo Lacerda <gustavolacerda655@gmail.com>
2020-05-07 11:05:08 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double rsalario, rservico, rcapital, gmedicos, geducacionais;
+
+    cout << "Renda anual com salario: ";
+    cin >> rsalario;
+    cout << "Renda anual com prestacao de servico: ";
+    cin >> rservico;
+    cout << "Renda anual com ganho de capital: ";
+    cin >> rcapital;
+    cout << "Gastos medicos: ";
+    cin >> gmedicos;
+    cout << "Gastos educacionais: ";
+    cin >> geducacionais;
+
     return 0;
 }
